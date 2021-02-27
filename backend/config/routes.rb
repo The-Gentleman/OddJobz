557831366fa2bf0e4e99c26ee4d55cf713a2efd4
The-Gentleman <zraja4402@gmail.com>
2021-02-27 11:40:51 -0500

    added routes + user index action

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :users
+  # resources :users
+  get '/users', to: 'users#index'
+  get '/users/:id', to: 'users#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
