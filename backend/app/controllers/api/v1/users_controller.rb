7a0a02c00d7bffc2b6f13460c4bd6a12cf680bcd
The-Gentleman <zraja4402@gmail.com>
2021-02-28 03:02:21 -0500

    added user serializer

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -2,12 +2,15 @@ class Api::V1::UsersController < ApplicationController
 
     def index 
         @users = User.all 
-        render json: @users
+        user_json = UserSerializer.new(@users).serialized_json
+        render json: user_json
     end 
 
     def show 
         @user = User.find(params[:id])
-        render json: @user
+        user_json = UserSerializer.new(@user).serialized_json
+        render json: user_json
+
     end 
 
 end
