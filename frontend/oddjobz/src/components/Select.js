85e84a66e37e71458af229d92e64ece6cee864d2
The-Gentleman <zraja4402@gmail.com>
2021-03-07 02:48:38 -0500

    currently trying to create dropdown menu of tasks

--- frontend/oddjobz/src/components/Select.js
+++ frontend/oddjobz/src/components/Select.js
@@ -4,6 +4,12 @@ export default function Select() {
     return (
         <div>
             <p>Please select a category</p>
+            <form>
+                <select>
+                </select>
+                <br></br>
+                <input id="submit" type="submit" name="submit" value="Find me a professional!" class="submit"></input>
+            </form>
         </div>
     )
 }
