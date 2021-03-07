b37fa2d7b78e20a3293e855b09da5c7f57aa68eb
The-Gentleman <zraja4402@gmail.com>
2021-03-06 22:00:54 -0500

    added select component

--- frontend/oddjobz/src/App.js
+++ frontend/oddjobz/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header'
-
+import Select from './components/Select'
 function App() {
   return (
     <div className="App">
+      {/* // 1 OF 5 STATELESS COMPONENTS */}
       <Header />
+      {/* // 2 OF 5 STATELESS COMPONENTS */}
+      <Select />
     </div>
   )
 }
