ad630f2ce0ce789caa43a20bde421c6bb512a879
The-Gentleman <zraja4402@gmail.com>
2021-03-18 15:24:16 -0400

    added basic routes

--- frontend/oddjobz/src/App.js
+++ frontend/oddjobz/src/App.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react'
 import './App.css';
 import Header from './components/Header'
 import Select from './components/Select'
-function App() {
-  return (
-    <div className="App">
-      {/* // 1 OF 5 STATELESS COMPONENTS */}
-      <Header />
-      <Select category={"category"} />
-    </div>
-  )
+import { BrowserRouter as Router, Route} from 'react-router-dom'
+
+
+export class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        {/* // 1 OF 5 STATELESS COMPONENTS */}
+        <Header />
+        <Select category={"category"} />
+        <Router>
+          <Route exact path ='/header' component={Header}/>
+        </Router>
+      </div>
+    )
+  }
 }
-export default App;
+
+export default App
+
