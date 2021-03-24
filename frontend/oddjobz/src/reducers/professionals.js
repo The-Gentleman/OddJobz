1502b3004930d174875b78e9ad601f588f15acf5
The-Gentleman <zraja4402@gmail.com>
2021-03-24 06:10:28 -0400

    added professional action + reducer

--- frontend/oddjobz/src/reducers/professionals.js
+++ frontend/oddjobz/src/reducers/professionals.js
@@ -1,5 +1,12 @@
-export default (state = [], action) => {
+const initialState = {
+    name: "",
+    profession: ""
+}
+
+export default (state = initialState, action) => {
     switch (action.type) {
+        case "SET_PROFESSIONAL":
+            return action.professional
         default:
             return state
     }
