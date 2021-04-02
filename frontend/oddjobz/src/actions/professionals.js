a5a2748fcf1aca5344cb705ea2b268d6ffd510fd
The-Gentleman <zraja4402@gmail.com>
2021-04-02 10:18:51 -0400

    added action creator and refactored

--- frontend/oddjobz/src/actions/professionals.js
+++ frontend/oddjobz/src/actions/professionals.js
@@ -1,6 +1,14 @@
-export const setProfessional = professional => {
-    return { 
-        type: "SET_PROFESSIONAL",
-        professional
+export const fetchProfessionals = () => {
+    return dispatch => {
+        fetch("http://localhost:3001/api/v1/professionals")
+        .then(response => response.json())
+        .then(professionals => {
+                dispatch({type: "SET_PROFESSIONAL", payload: professionals});
+                professionals.data.map(professional => {
+                    return professional.attributes.name
+                })
+        })
     }
 }
+
+
